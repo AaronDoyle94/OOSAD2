d8b2b292a51e96d09a794e42ed5104eb592c2e12
AaronDoyle94 <zippo.wow@hotmail.com>
2017-01-31 14:44:08 +0000

    LecturerAndJava

---
+++ week2/Week2Lecturer/Driver.java
@@ -0,0 +1,16 @@
+package Week2Lecturer;
+
+public class Driver {
+
+	private int testStudent;
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public void main(String[] args) {
+		// TODO - implement Driver.main
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ week2/Week2Lecturer/Student.java
@@ -0,0 +1,13 @@
+package Week2Lecturer;
+
+public class Student {
+
+	Module module;
+	private Module students;
+
+	public Student() {
+		// TODO - implement Student.Student
+		throw new UnsupportedOperationException();
+	}
+
+}
